09f8ac6a033f0d4ea78d69f62631aeb9bd680ab8
Marc Diallo <mark.devguru@outlook.com>
2026-06-03 11:24:19 -0500

    Refactor book card markup structure

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function Book(title, author, pages) {
 function addBookToLibrary(title, author, pages) {
   myLibrary.push(new Book(title, author, pages));
 }
+
+addBookToLibrary("Breath", "James N.", 210);
+
+addBookToLibrary("Harry Potter", "J.K. Rowling", 320);
+
+const myLibraryBook = function (library) {
+  library.forEach(function (el, i, arr) {
+    console.log(el);
+  });
+};
